980c9ca72c06b92ca921ab4b05df8a85a7b7c8a1
cfowl <goose_7@msn.com>
2022-07-05 23:41:44 -0600

    landing page back to ejs

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const routes = require('express').Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-// Landing page for all guests
-routes.get('/', ensureGuest, (req,res) => {
-    let html = '<h1>Jewelry Cataloger API Login</h1>';
-    html += '<p>Log in with your Google Account to see the api-docs.</p>';
-    html += `<button onclick="window.location.href='/auth/google';">Log In</button>`;
-    res.send(html);
-})
-
 routes.use('/api-docs', ensureAuth, require('./swagger'));
 routes.use('/user', ensureAuth, require('./user'));
 routes.use('/insurance', ensureAuth, require('./insurance'));
@@ -16,11 +8,12 @@ routes.use('/jewelry', ensureAuth, require('./jewelry'));
 routes.use('/market', ensureAuth, require('./market'));
 routes.use('/auth', require('./auth'));
 
-// routes.get('/', (req, res) => {
-// 	res.render('index', {
-// 		title: 'Welcome to Jewelry Cataloger',
-// 		//   isAuthenticated: req.oidc.isAuthenticated(),
-// 	});
-// });
+// Landing page for guests
+routes.get('/', ensureGuest, (req, res) => {
+	res.render('index', {
+		title: 'Welcome to Jewelry Cataloger',
+		//   isAuthenticated: req.oidc.isAuthenticated(),
+	});
+});
 
 module.exports = routes;
