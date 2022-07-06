b072d93407a72075a303dbc87994fe651f9c5255
cfowl <goose_7@msn.com>
2022-07-05 23:18:46 -0600

    removed unused code

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
-const { auth } = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
 
-
-router.use('/api-docs', requiresAuth(), swaggerUi.serve);
-router.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
