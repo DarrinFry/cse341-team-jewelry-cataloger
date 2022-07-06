a0bf29326391b033ae7e8032a2f5bf6fd451d22f
cfowl <goose_7@msn.com>
2022-07-05 23:19:13 -0600

    added auth route and landing page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const routes = require('express').Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-routes.use('/', require('./swagger'));
-routes.use('/user', require('./user'));
-routes.use('/insurance', require('./insurance'));
-routes.use('/jewelry', require('./jewelry'));
-routes.use('/market', require('./market'));
+// Landing page for all guests
+routes.get('/', ensureGuest, (req,res) => {
+    let html = '<h1>Jewelry Cataloger API Login</h1>';
+    html += '<p>Log in with your Google Account to see the api-docs.</p>';
+    html += `<button onclick="window.location.href='/auth/google';">Log In</button>`;
+    res.send(html);
+})
 
-routes.get('/', (req, res) => {
-	res.render('index', {
-		title: 'Welcome to Jewelry Cataloger',
-		//   isAuthenticated: req.oidc.isAuthenticated(),
-	});
-});
+routes.use('/api-docs', ensureAuth, require('./swagger'));
+routes.use('/user', ensureAuth, require('./user'));
+routes.use('/insurance', ensureAuth, require('./insurance'));
+routes.use('/jewelry', ensureAuth, require('./jewelry'));
+routes.use('/market', ensureAuth, require('./market'));
+routes.use('/auth', require('./auth'));
+
+// routes.get('/', (req, res) => {
+// 	res.render('index', {
+// 		title: 'Welcome to Jewelry Cataloger',
+// 		//   isAuthenticated: req.oidc.isAuthenticated(),
+// 	});
+// });
 
 module.exports = routes;
