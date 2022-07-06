544f19bf63d9d5f845f65e00ec1ed73e573ab7e0
cfowl <goose_7@msn.com>
2022-07-05 23:19:59 -0600

    added passport, sessions, comments

--- server.js
+++ server.js
@@ -2,30 +2,46 @@ const express = require('express');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
-
 const swaggerAutogen = require('swagger-autogen');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
-
 const bodyParser = require('body-parser');
-
 const dotenv = require('dotenv').config();
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./db/connect');
+
+// Passport config
+require('./config/passport')(passport);
 
 // Connect the database
 connectDB();
 
 const port = process.env.PORT || 8080;
-
 const app = express();
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_DB_URI }),
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 }
+}));
+
+// Passport middleware
+app
+  .use(passport.initialize())
+  .use(passport.session());
+
+// EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
-// const uri = process.env.MONGO_DB_URI;
 
+// App
 app
-	.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 	.use(bodyParser.json())
 	.use((req, res, next) => {
 		res.setHeader('Access-Control-Allow-Origin', '*');
@@ -43,17 +59,10 @@ app
 	})
 	.use('/', require('./routes'));
 
+// Handle any uncaught exceptions
 process.on('uncaughtException', (err, origin) => {
 	console.log(process.stderr.fd, `Exception: ${err}\n` + `Origin: ${origin}`);
 });
 
+// Start app
 app.listen(port, console.log(`Server running on port ${port}`));
-
-// mongodb.initDb((err, mongodb) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     app.listen(port);
-//     console.log(`Connected to DB and listening on ${port}`);
-//   }
-// });
